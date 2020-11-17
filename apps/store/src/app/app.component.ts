4494c896eacdbc1d9588512123d6226b7f11204b
hendersonanalytics <everett@hendersonanalytics.com>
2020-11-17 17:05:26 -0600

    end lab 8

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,7 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
-import { getAllGames } from '../fake-api';
 import { formatRating } from '@bg-hoard/store/util-formatters';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'bg-hoard-root',
@@ -9,13 +10,18 @@ import { formatRating } from '@bg-hoard/store/util-formatters';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) { }
   title = 'store';
-  public games;
+  public games$;
   public game;
 
   ngOnInit() {
-    this.games = getAllGames().map((game) => {
-      return {...game, rating: formatRating(game.rating)};
-    });
+    this.games$ = this.http.get('/api/games').pipe(
+      map((games: any[]) => {
+        return games.map(game => {
+          return {...game, rating: formatRating(game.rating)}
+        });
+      })
+    );
   }
 }
--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -1,27 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 
 import { MatCardModule } from '@angular/material/card';
 import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  {
+    path: 'game/:id',
+    loadChildren: () => import('@bg-hoard/store/feature-game-detail').then(m => m.StoreFeatureGameDetailModule)
+  }
+]
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CommonModule,
     BrowserModule,
+    HttpClientModule,
     MatCardModule,
     StoreUiSharedModule,
-    RouterModule.forRoot([
-      {
-        path: 'game/:id', // <---- HERE
-        loadChildren: () =>
-            import('@bg-hoard/store/feature-game-detail').then(m => m.StoreFeatureGameDetailModule)
-        }
-    ], { initialNavigation: 'enabled' }),
+    RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
   ],
   providers: [],
   bootstrap: [AppComponent],

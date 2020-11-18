d5a7c6beec573eccd526e618d2a08644276ae129
hendersonanalytics <everett@hendersonanalytics.com>
2020-11-17 22:05:36 -0600

    end lab 10

---
+++ libs/store/ui-shared/src/lib/header/header.component.stories.ts
@@ -0,0 +1,19 @@
+import { MatToolbarModule } from '@angular/material/toolbar';
+import '@angular/material/prebuilt-themes/deeppurple-amber.css';
+
+import { text, number, boolean } from '@storybook/addon-knobs';
+import { HeaderComponent } from './header.component';
+
+export default {
+  title: 'HeaderComponent'
+}
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: [MatToolbarModule]
+  },
+  component: HeaderComponent,
+  props: {
+    title: text('title', ''),
+  }
+})
--- libs/store/ui-shared/src/lib/store-ui-shared.module.ts
+++ libs/store/ui-shared/src/lib/store-ui-shared.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { HeaderComponent } from '../../header/header.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [CommonModule, MatToolbarModule],
